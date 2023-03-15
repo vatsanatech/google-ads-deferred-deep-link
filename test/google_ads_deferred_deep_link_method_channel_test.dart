65af17361f1351c0040183d929da9ba3864c4a8c
zhanglei <lei.zhang@lejent.com>
2023-03-15 16:02:04 +0800

    :white_check_mark: update test

--- test/google_ads_deferred_deep_link_method_channel_test.dart
+++ test/google_ads_deferred_deep_link_method_channel_test.dart
@@ -3,8 +3,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:google_ads_deferred_deep_link/google_ads_deferred_deep_link_method_channel.dart';
 
 void main() {
-  MethodChannelGoogleAdsDeferredDeepLink platform = MethodChannelGoogleAdsDeferredDeepLink();
-  const MethodChannel channel = MethodChannel('com.github/google_ads_deferred_deep_link');
+  TestWidgetsFlutterBinding.ensureInitialized();
+  MethodChannelGoogleAdsDeferredDeepLink platform =
+      MethodChannelGoogleAdsDeferredDeepLink();
+  const MethodChannel channel =
+      MethodChannel('com.github/google_ads_deferred_deep_link');
 
   TestWidgetsFlutterBinding.ensureInitialized();
 
@@ -18,7 +21,8 @@ void main() {
     channel.setMockMethodCallHandler(null);
   });
 
-  test('getPlatformVersion', () async {
+  test('startFetch', () async {
+    platform.deferredDeepLinkStream.listen((event) {});
     expect(await platform.startFetch(), 'Success');
   });
 }
--- test/google_ads_deferred_deep_link_test.dart
+++ test/google_ads_deferred_deep_link_test.dart
@@ -1,32 +1,43 @@
+import 'dart:async';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:google_ads_deferred_deep_link/google_ads_deferred_deep_link.dart';
 import 'package:google_ads_deferred_deep_link/google_ads_deferred_deep_link_platform_interface.dart';
 import 'package:google_ads_deferred_deep_link/google_ads_deferred_deep_link_method_channel.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
-class MockGoogleAdsDeferredDeepLinkPlatform 
+class MockGoogleAdsDeferredDeepLinkPlatform
     with MockPlatformInterfaceMixin
     implements GoogleAdsDeferredDeepLinkPlatform {
+  final StreamController<GoogleAdsDeferredDeepLinkResult> _streamController =
+      StreamController<GoogleAdsDeferredDeepLinkResult>();
 
   @override
-  Stream<GoogleAdsDeferredDeepLinkResult?> get deferredDeepLinkStream => throw UnimplementedError();
+  Stream<GoogleAdsDeferredDeepLinkResult?> get deferredDeepLinkStream =>
+      _streamController.stream;
 
   @override
   Future<String?> startFetch() => Future.value('Success');
 }
 
 void main() {
-  final GoogleAdsDeferredDeepLinkPlatform initialPlatform = GoogleAdsDeferredDeepLinkPlatform.instance;
+  TestWidgetsFlutterBinding.ensureInitialized();
+  final GoogleAdsDeferredDeepLinkPlatform initialPlatform =
+      GoogleAdsDeferredDeepLinkPlatform.instance;
 
   test('$MethodChannelGoogleAdsDeferredDeepLink is the default instance', () {
-    expect(initialPlatform, isInstanceOf<MethodChannelGoogleAdsDeferredDeepLink>());
+    expect(initialPlatform,
+        isInstanceOf<MethodChannelGoogleAdsDeferredDeepLink>());
   });
 
-  test('getPlatformVersion', () async {
-    GoogleAdsDeferredDeepLink googleAdsDeferredDeepLinkPlugin = GoogleAdsDeferredDeepLink();
-    MockGoogleAdsDeferredDeepLinkPlatform fakePlatform = MockGoogleAdsDeferredDeepLinkPlatform();
+  test('startFetch', () async {
+    GoogleAdsDeferredDeepLink googleAdsDeferredDeepLinkPlugin =
+        GoogleAdsDeferredDeepLink();
+    MockGoogleAdsDeferredDeepLinkPlatform fakePlatform =
+        MockGoogleAdsDeferredDeepLinkPlatform();
     GoogleAdsDeferredDeepLinkPlatform.instance = fakePlatform;
-  
+
+    googleAdsDeferredDeepLinkPlugin.deferredDeepLinkStream.listen((event) {});
     expect(await googleAdsDeferredDeepLinkPlugin.startFetch(), 'Success');
   });
 }
